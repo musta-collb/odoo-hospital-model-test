6e3297bb3ab0f4100a66854d12d5d5b05f5ddcc5
Mustapha <musatapha2000@gmail.com>
2021-08-09 10:52:26 +0100

    start the model

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'hospital',
+    'version': '1.0',
+    'category': '',
+    'description': """hospital management""",
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
